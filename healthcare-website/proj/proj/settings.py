3086c21d45b8461ea47431831a8f044a39e524ea
AndroDev21 <77734421+AndroDev21@users.noreply.github.com>
2022-04-03 15:40:18 +0530

    Update settings.py

--- healthcare-website/proj/proj/settings.py
+++ healthcare-website/proj/proj/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)%d1&wt-pblgmd-5^c@b@hvsu3d1)b&5%iu*kf2s**j_too088'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
